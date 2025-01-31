2271cbc15ed2ea7642bec86d552396d949347889
Laloeyudik <99laluwahyudi@gmail.com>
2025-01-31 20:50:47 +0800

    [backend] Update Structere

--- frontend-naznas/src/elements/IconLayanan.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function IconLayanan({icon, className="", ...props}) {
-  return (
-    <div {...props} className={`bg-Primary-500 w-16 h-16 rounded-full flex justify-center items-center` + className}>
-        <img src={icon} alt="" className='h-10'/>
-    </div>
-  )
-}
